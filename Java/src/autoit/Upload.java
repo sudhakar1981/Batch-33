ae5c10d5c18fa3e25957988b8beb89a293cb8a27
Manoj <manoj@gmail.com>
2018-06-13 23:34:46 +0530

    Upload the project

--- Java/src/autoit/Upload.java
+++ Java/src/autoit/Upload.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Upload {
+public class Upload  {
 
 	public static void main(String[] args) throws IOException {
 		
