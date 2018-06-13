a5ba36df81c5a435d08ec67df7cf0ee1a6d26f2e
Manoj <manoj@gmail.com>
2018-06-13 23:13:57 +0530

    dowload is added.

--- Java/src/autoit/Download.java
+++ Java/src/autoit/Download.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Download {
+public class Download  {
 
 	public static void main(String[] args) throws IOException {
 
